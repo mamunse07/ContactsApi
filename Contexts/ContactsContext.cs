469a722d5d52826db02cde95f196983603b0da71
mamun69 <mamun007j69@gmail.com>
2018-02-06 19:37:43 +0600

    ContactApi pro added to git

---
+++ Contexts/ContactsContext.cs
@@ -0,0 +1,19 @@
+using ContactsApi.Models;
+using Microsoft.EntityFrameworkCore;
+ 
+namespace ContactsApi.Contexts
+{
+    public class ContactsContext : DbContext
+    {
+        public ContactsContext(DbContextOptions<ContactsContext> options)
+            :base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=.\\SQLEXPRESS;Initial Catalog=ContactsDB;Integrated Security=True;MultipleActiveResultSets=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+        public ContactsContext(){ }
+        public DbSet<Contacts> Contacts { get; set; }
+    }
+}
